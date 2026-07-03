238d03eb55c2a64a3fc5af57d00d662a8845d42e
Victor Ivanisov <victor.ivanisov.dev@gmail.com>
2026-07-03 16:24:08 +0300

    ci: set dynamic headless mode based on CI environment

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   
   use: {
     baseURL: 'https://islam.social',
-    headless: false,
+    headless: process.env.CI ? true : false,
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
